e33fbba98736dcd0e80a86e32df00915729aa34f
PrasannaJung <prasannajung08@gmail.com>
2025-02-04 21:07:12 +0545

    Included limiter middleware

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -7,17 +7,20 @@ const {
   resendOtp,
   getUser,
   updateUser,
+  changePassword,
 } = require("../controllers/auth.controller");
 const validate = require("../middlewares/validate.middleware");
 const { signupSchema } = require("../validators/auth.validator");
 const loginLimiter = require("../middlewares/loginLimiter.middleware");
 const isAuthenticated = require("../middlewares/isAuthenticated.middleware");
+const recaptchaMiddleware = require("../middlewares/recpatcha.middleware");
 
 router.post("/signup", validate(signupSchema), signupUser);
-router.post("/login", loginUser);
+router.post("/login", recaptchaMiddleware, loginUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/resend-otp", resendOtp);
 router.get("/me", isAuthenticated, getUser);
 router.post("/update", isAuthenticated, updateUser);
+router.post("/change-password", changePassword);
 
 module.exports = router;
