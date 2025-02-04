492e66315efd739df7412317aba95e093ab71c9d
PrasannaJung <prasannajung08@gmail.com>
2025-02-04 21:07:27 +0545

    Update routes in service

--- server/src/routes/service.routes.js
+++ server/src/routes/service.routes.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
 
 const serviceController = require("../controllers/service.controller");
+const isAdmin = require("../middlewares/isAdmin.middleware");
+const isAuthenticated = require("../middlewares/isAuthenticated.middleware");
 
 router.post("/create", serviceController.createService);
 router.get("/", serviceController.getAllServices);
 router.get("/search", serviceController.getAllServicesNameAndId);
 router.get("/:id", serviceController.getSingleService);
+router.put(
+  "/update/:id",
+  isAuthenticated,
+  isAdmin,
+  serviceController.updateServiceStatus,
+);
 
 module.exports = router;
