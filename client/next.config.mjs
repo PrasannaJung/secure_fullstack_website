38d9fd37acca7d1b623339858cf18ffb463c144b
PrasannaJung <prasannajung08@gmail.com>
2025-02-04 21:13:16 +0545

    Added config to include local domains and ssl

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,21 @@
+import fs from "fs";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // allow images to be imported from unsplash
+
+  // Allow images from external sources
   images: {
-    domains: ["images.unsplash.com", "https://randomuser.me", "localhost"],
+    domains: ["images.unsplash.com", "randomuser.me", "localhost"],
+  },
+
+  // Enable HTTPS in local dev
+  devServer: {
+    https: {
+      key: fs.readFileSync(path.resolve("./ssl/privkey.pem")),
+      cert: fs.readFileSync(path.resolve("./ssl/cert.pem")),
+    },
   },
 };
 
